e42b7b3a49954520e222ad09117fdda5d99c189c
nryn <narayanohanlon@gmail.com>
2017-02-16 15:45:25 +0000

    added private methods to check docking station statuses

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,17 +7,26 @@ class DockingStation
   end
 
   def release_bike
-    fail "Sorry, no bikes left!" unless !docked_bikes.empty?
+    fail "Sorry, no bikes left!" if empty?
     docked_bikes.shift
     #return docked_bikes unless docked_bikes.empty?
     #raise "Sorry, no bikes left!"
   end
 
   def dock(bike)
-    fail "Sorry, no space left!" unless docked_bikes.length < 20
+    fail "Sorry, no space left!" if full?
     #return raise "Sorry, no space left!" if docked_bikes
     @docked_bikes << bike
     docked_bikes.last
   end
 
+  private
+
+  def full?
+    docked_bikes.length >= 20
+  end
+
+  def empty?
+    docked_bikes.empty?
+  end
 end
