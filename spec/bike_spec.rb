fea4bb2aa0c779f3e4b7ef02c0b91fc5328bebe4
Ashwini <ashie.oh@gmail.com>
2017-02-16 06:21:21 +0000

    Updated bike class and spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,10 +1,11 @@
 require 'bike'
 
 describe Bike do
-it {is_expected.to respond_to :working?}
-it {is_expected.to be_working}
-#it 'works' do
-# bike = Bike.new
-# expect(bike).to be_working
-#end
+  it {is_expected.to respond_to :working?} # check if bike as a method called working?
+  it {is_expected.to be_working} # using predicate matchers to check if working? returns true
+
+  #it 'works' do
+    # bike = Bike.new
+    # expect(bike).to be_working
+  #end
 end
