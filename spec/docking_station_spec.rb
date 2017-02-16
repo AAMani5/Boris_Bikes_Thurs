aeb520baee157c9d99de053b584cae07b9d766ae
Ashwini <ashie.oh@gmail.com>
2017-02-16 06:58:57 +0000

    Updated the context

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,34 +19,34 @@ describe DockingStation do
 
   #it { expect(docking_station.release_bike).to be_an_instance_of Bike} # release_bike should return a Bike type
 
-  it 'returns a bike object, if docking station has one'do
+  it 'returns a bike, if docking station has one'do
     # bike1 = Bike.new
     docking_station.dock(@bike)
     expect(docking_station.release_bike).to eq @bike
 
   end
 
-  it "return an error message if dock is nil" do
+  it "raises an error if it's empty" do
     # docking_station.dock(nil) # we dont't need this as its nil to begin with
     expect {docking_station.release_bike}.to raise_error("Sorry, no bikes left!") #if docking_station.docked_bikes == nil
   end
 
   it {is_expected.to respond_to :dock} # should have a dock method
 
-  it "check if docking station is receiving a bike when dock is called" do
+  it "receives a bike when docked" do
     # bike = Bike.new
     expect(docking_station.dock(@bike)).to eq @bike
     allow(docking_station).to receive(:dock).with(an_instance_of(Bike)) {raise "station not receiving a Bike object"}
   end
 
-  it "return an error message if dock is full" do
+  it "returns an error message if it's full" do
     # bike = Bike.new
     docking_station.dock(@bike)
     expect {docking_station.dock(@bike)}.to raise_error("Sorry, no space left!") #if docking_station.docked_bikes has a one Bike type
   end
 
   it {is_expected.to respond_to :docked_bikes}
-  it "docking station should return a bike when docked_bikes is called" do
+  it "displays bike that is already docked" do
     # bike = Bike.new
     docking_station.dock(@bike)
     expect(docking_station.docked_bikes).to eq @bike
