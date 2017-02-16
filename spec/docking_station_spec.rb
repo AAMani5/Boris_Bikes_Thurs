08949305fa7a4396c38bd1b01d3fbb871f89ffce
Ashwini <ashie.oh@gmail.com>
2017-02-16 06:52:12 +0000

    Used 'before do..end' and 'alias_method'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,47 +1,55 @@
 require 'docking_station'
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike} # should have a release_bike method
 
-  #it { expect(subject.release_bike).to be_an_instance_of Bike}
-
-  it 'docking station return a bike, which is working'do
-    bike1 = Bike.new
-    subject.dock(bike1)
-    bike2 = subject.release_bike
-    expect(bike2).to eq bike1
-
+  before(:all) do
+    @bike = Bike.new
   end
 
+  alias_method :docking_station, :subject
+
+  ### Bike related tests - why do we have it in docking_station_spec?
   it 'bike responds to working and returns true' do
-    bike = Bike.new
-    expect(bike).to be_working
+    # bike = Bike.new # dont only once in the begining
+    expect(@bike).to be_working
+  end
+
+  ### DockingStation related tests
+  it {is_expected.to respond_to :release_bike} # should have a release_bike method
+
+  #it { expect(docking_station.release_bike).to be_an_instance_of Bike} # release_bike should return a Bike type
+
+  it 'returns a bike object, if docking station has one'do
+    # bike1 = Bike.new
+    docking_station.dock(@bike)
+    expect(docking_station.release_bike).to eq @bike
+
   end
 
   it "return an error message if dock is nil" do
-    subject.dock(nil)
-    expect {subject.release_bike}.to raise_error("Sorry, no bikes left!") #if subject.docked_bikes == nil
+    # docking_station.dock(nil) # we dont't need this as its nil to begin with
+    expect {docking_station.release_bike}.to raise_error("Sorry, no bikes left!") #if docking_station.docked_bikes == nil
   end
 
   it {is_expected.to respond_to :dock} # should have a dock method
 
   it "check if docking station is receiving a bike when dock is called" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-    allow(subject).to receive(:dock).with(an_instance_of(Bike)) {raise "station not receiving a Bike object"}
+    # bike = Bike.new
+    expect(docking_station.dock(@bike)).to eq @bike
+    allow(docking_station).to receive(:dock).with(an_instance_of(Bike)) {raise "station not receiving a Bike object"}
   end
 
   it "return an error message if dock is full" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect {subject.dock(bike)}.to raise_error("Sorry, no space left!") #if subject.docked_bikes == nil
+    # bike = Bike.new
+    docking_station.dock(@bike)
+    expect {docking_station.dock(@bike)}.to raise_error("Sorry, no space left!") #if docking_station.docked_bikes has a one Bike type
   end
 
   it {is_expected.to respond_to :docked_bikes}
   it "docking station should return a bike when docked_bikes is called" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.docked_bikes).to eq bike
+    # bike = Bike.new
+    docking_station.dock(@bike)
+    expect(docking_station.docked_bikes).to eq @bike
   end
 
 end
