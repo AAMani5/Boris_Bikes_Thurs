8b543eb4d7e4616edbea346b7e6ad5ec7462caf3
Ashwini <ashie.oh@gmail.com>
2017-02-16 06:22:14 +0000

    Removed an unnecessary method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,6 @@
 # docking_station = DockingStation
 class DockingStation
-  attr_reader :docked_bikes
+  attr_reader :docked_bikes 
 
   def release_bike
     return docked_bikes unless docked_bikes.nil?
@@ -10,10 +10,6 @@ class DockingStation
   def dock(bike)
     return raise "Sorry, no space left!" if docked_bikes
     @docked_bikes = bike
-  end
-
-  def show_docked
-    docked_bikes
   end
 
 end
