891409c3956b0ff5ba03681a97e09d6b660f2d84
Ashwini <ashie.oh@gmail.com>
2017-02-18 19:45:08 +0000

    Test drive creation of add_bike method

--- spec/support/shared_examples_for_bike_container.rb
+++ spec/support/shared_examples_for_bike_container.rb
@@ -14,4 +14,18 @@ shared_examples_for BikeContainer do
 
   end
 
+  describe 'add_bike' do
+
+    it 'receives a bike' do
+      subject.add_bike double :bike
+      expect(subject).not_to be_empty
+    end
+
+    it 'raises an error when full' do
+      subject.capacity.times {subject.add_bike double :bike}
+      expect {subject.add_bike double :bike}.to raise_error "#{described_class.name} full"
+    end
+
+  end
+
 end
